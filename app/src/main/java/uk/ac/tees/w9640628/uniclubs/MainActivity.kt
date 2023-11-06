9b7f193191d284817a3dfc0b8be1cfe1ca6c72f4
Yashu333 <saichintakayala4@gmail.com>
2023-11-06 17:04:20 +0000

    Added a splash screen to the app

--- app/src/main/java/uk/ac/tees/w9640628/uniclubs/MainActivity.kt
+++ app/src/main/java/uk/ac/tees/w9640628/uniclubs/MainActivity.kt
@@ -3,13 +3,20 @@ package uk.ac.tees.w9640628.uniclubs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import uk.ac.tees.w9640628.uniclubs.ui.theme.UniClubsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    UniClubs()
                 }
             }
         }
@@ -30,17 +37,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun UniClubs(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
-    )
+            .fillMaxSize()
+            .background(Color(237, 129, 49))
+    ) {
+        Text(
+            text = stringResource(R.string.app_title),
+            fontSize = 40.sp,
+            color = Color(255,255,255)
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun UniClubsPreview() {
     UniClubsTheme {
-        Greeting("Android")
+        UniClubs()
     }
 }
